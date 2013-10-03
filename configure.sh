78ea856904923d6f64f1ea594420ebbe46401d1f
dextero <hukutizuviki+git@gmail.com>
2013-10-03 17:07:19 +0200

    added switches for partial execution

--- configure.sh
+++ configure.sh
@@ -5,12 +5,53 @@ if [ "${LOGNAME}" != "root" ]; then
     exit 1
 fi
 
-if [ $# -gt 0 ]; then
+# arguments
+ALL_ARGUMENTS=$@
+CUSTOM_ARGUMENTS="no"
+
+WITH_ADD_REPOSITORIES="no"
+WITH_INSTALL_PACKAGES="no"
+WITH_CONFIGURE_MOUNT="no"
+WITH_SET_SHELL="no"
+WITH_CONFIGURE_AGH_WPA="no"
+
+LOGNAME=""
+
+while [ $# -gt 0 ]; do
     if [ -e "/home/$1" ]; then
         LOGNAME="$1"
+    elif [ "$1" = "--add-repositories" ]; then
+        WITH_ADD_REPOSITORIES="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--install-packages" ]; then
+        WITH_INSTALL_PACKAGES="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--configure-mount" ]; then
+        WITH_CONFIGURE_MOUNT="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--set-shell" ]; then
+        WITH_SET_SHELL="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--configure-agh-wpa" ]; then
+        WITH_CONFIGURE_AGH_WPA="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "${1:0:2}" = "--" ]; then
+        CUSTOM_ARGUMENTS="yes"
     fi
-else
+
+    shift
+done
+
+if [ -z "${LOGNAME}" ]; then
     LOGNAME="dex"
+fi
+
+if [ "${CUSTOM_ARGUMENTS}" = "no" ]; then
+    WITH_ADD_REPOSITORIES="yes"
+    WITH_INSTALL_PACKAGES="yes"
+    WITH_CONFIGURE_MOUNT="yes"
+    WITH_SET_SHELL="yes"
+    WITH_CONFIGURE_AGH_WPA="yes"
 fi
 
 echo ".--------------"
@@ -34,137 +75,149 @@ function fail {
 }
 
 # add repositories
-RELEASE_NAME=`lsb_release -sc`
-echo "# adding repositories"
+if [ "${WITH_ADD_REPOSITORIES}" = "yes" ]; then
+    RELEASE_NAME=`lsb_release -sc`
+    echo "# adding repositories"
 
-## Dropbox
-if [ ! -e /etc/apt/sources.list.d/dropbox.list ]; then
-    apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E >> ${INSTALL_LOGFILE} 2>&1
-    echo "deb http://linux.dropbox.com/ubuntu ${RELEASE_NAME} main" > /etc/apt/sources.list.d/dropbox.list
+    ## Dropbox
+    if [ ! -e /etc/apt/sources.list.d/dropbox.list ]; then
+        apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E >> ${INSTALL_LOGFILE} 2>&1
+        echo "deb http://linux.dropbox.com/ubuntu ${RELEASE_NAME} main" > /etc/apt/sources.list.d/dropbox.list
+    fi
+
+    ## Final Term
+    yes | add-apt-repository ppa:finalterm/daily >> ${INSTALL_LOGFILE} 2>&1
+
+    ## Google
+    if [ ! -e /etc/apt/sources.list.d/google.list ]; then
+        wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - >> ${INSTALL_LOGFILE} 2>&1
+        echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
+    fi
+
+    ## Skype
+    if [ ! -e /etc/apt/sources.list.d/canonical_partner.list ]; then
+        echo "deb http://archive.canonical.com/ubuntu/ ${RELEASE_NAME} partner" >> /etc/apt/sources.list.d/canonical_partner.list
+    fi
+
+    ## Spotify
+    if [ ! -e /etc/apt/sources.list.d/spotify.list ]; then
+        apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 >> ${INSTALL_LOGFILE} 2>&1
+        echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list
+    fi
+
+    ## Vim
+    yes | add-apt-repository ppa:dgadomski/vim-daily >> ${INSTALL_LOGFILE} 2>&1
+
+    echo "# running apt-get update"
+    apt-get update >> ${INSTALL_LOGFILE} 2>&1
 fi
-
-## Final Term
-yes | add-apt-repository ppa:finalterm/daily >> ${INSTALL_LOGFILE} 2>&1
-
-## Google
-if [ ! -e /etc/apt/sources.list.d/google.list ]; then
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - >> ${INSTALL_LOGFILE} 2>&1
-    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
-fi
-
-## Skype
-if [ ! -e /etc/apt/sources.list.d/canonical_partner.list ]; then
-    echo "deb http://archive.canonical.com/ubuntu/ ${RELEASE_NAME} partner" >> /etc/apt/sources.list.d/canonical_partner.list
-fi
-
-## Spotify
-if [ ! -e /etc/apt/sources.list.d/spotify.list ]; then
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 >> ${INSTALL_LOGFILE} 2>&1
-    echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list
-fi
-
-## Vim
-yes | add-apt-repository ppa:dgadomski/vim-daily >> ${INSTALL_LOGFILE} 2>&1
-
-echo "# running apt-get update"
-apt-get update >> ${INSTALL_LOGFILE} 2>&1
 
 # install essential packages
-echo "# installing packages"
+if [ "${WITH_INSTALL_PACKAGES}" = "yes" ]; then
+    echo "# installing packages"
 
-PACKAGES=" \
-    build-essential \
-    clang \
-    cmake \
-    dropbox \
-    finalterm \
-    git \
-    gnome-shell \
-    google-chrome-stable \
-    mercurial \
-    nvidia-current \
-    pidgin \
-    powertop \
-    python \
-    python-dev \
-    skype \
-    spotify-client \
-    subversion \
-    thunderbird \
-    vim-gtk \
-    virtualbox \
-    zsh \
-"
+    PACKAGES=" \
+        build-essential \
+        clang \
+        cmake \
+        dropbox \
+        finalterm \
+        git \
+        gnome-shell \
+        google-chrome-stable \
+        mercurial \
+        nvidia-current \
+        pidgin \
+        powertop \
+        python \
+        python-dev \
+        skype \
+        spotify-client \
+        subversion \
+        thunderbird \
+        valgrind \
+        vim-gtk \
+        virtualbox \
+        zsh \
+    "
 
-for PACKAGE in ${PACKAGES}; do
-    echo "   * ${PACKAGE}"
-    apt-get install -y ${PACKAGE} >> ${INSTALL_LOGFILE} 2>&1
-done
+    for PACKAGE in ${PACKAGES}; do
+        echo "   * ${PACKAGE}"
+        apt-get install -y ${PACKAGE} >> ${INSTALL_LOGFILE} 2>&1
+    done
+fi
 
 # auto-mount data partition
 DATA_PARTITION_PATH="/dev/sda2"
 DATA_PARTITION_MOUNT_PATH="/media/D"
-DATA_PARTITION_FILE_SYSTEM=`blkid ${DATA_PARTITION_PATH} | awk '{ gsub(/^.*TYPE="/, ""); gsub(/".*$/, ""); print }'`
 
-if [ `mount | grep "^${DATA_PARTITION_PATH}" | wc -l` -gt 0 ]; then
-    echo "# updating /etc/fstab skipped: ${DATA_PARTITION_PATH} already mounted"
-else
-    echo -n "# adding ${DATA_PARTITION_PATH} (${DATA_PARTITION_FILE_SYSTEM}, mount at ${DATA_PARTITION_MOUNT_PATH}) to /etc/fstab..."
+if [ "${WITH_CONFIGURE_MOUNT}" = "yes" ]; then
+    DATA_PARTITION_FILE_SYSTEM=`blkid ${DATA_PARTITION_PATH} | awk '{ gsub(/^.*TYPE="/, ""); gsub(/".*$/, ""); print }'`
 
-    mkdir -p ${DATA_PARTITION_MOUNT_PATH}
-    echo "${DATA_PARTITION_PATH} ${DATA_PARTITION_MOUNT_PATH} ${DATA_PARTITION_FILE_SYSTEM} errors=remount-ro 0 0" >> /etc/fstab
-
-    if mount -a; then
-        echo " ok"
+    if [ `mount | grep "^${DATA_PARTITION_PATH}" | wc -l` -gt 0 ]; then
+        echo "# updating /etc/fstab skipped: ${DATA_PARTITION_PATH} already mounted"
     else
-        fail "mount -a failed"
+        echo -n "# adding ${DATA_PARTITION_PATH} (${DATA_PARTITION_FILE_SYSTEM}, mount at ${DATA_PARTITION_MOUNT_PATH}) to /etc/fstab..."
+
+        mkdir -p ${DATA_PARTITION_MOUNT_PATH}
+        echo "${DATA_PARTITION_PATH} ${DATA_PARTITION_MOUNT_PATH} ${DATA_PARTITION_FILE_SYSTEM} errors=remount-ro 0 0" >> /etc/fstab
+
+        if mount -a; then
+            echo " ok"
+        else
+            fail "mount -a failed"
+        fi
     fi
 fi
 
 # set default shell
-DEFAULT_SHELL=`which zsh`
-echo -n "# setting default shell to ${DEFAULT_SHELL}..."
-if chsh --shell "${DEFAULT_SHELL}" "${LOGNAME}"; then
-    echo " ok"
-else
-    fail "chsh '${DEFAULT_SHELL}' failed"
+if [ "${WITH_SET_SHELL}" = "yes" ]; then
+    DEFAULT_SHELL=`which zsh`
+    echo -n "# setting default shell to ${DEFAULT_SHELL}..."
+    if chsh --shell "${DEFAULT_SHELL}" "${LOGNAME}"; then
+        echo " ok"
+    else
+        fail "chsh '${DEFAULT_SHELL}' failed"
+    fi
 fi
 
 # configure AGH-WPA WiFi network
-echo "# configuring AGH-WPA network"
-CERT_URL="https://panel.agh.edu.pl/CA-AGH/CA-AGH.der"
-CERT_DIR="/etc/ca-certificates"
-AGH_WPA_OUTPUT="/etc/NetworkManager/system-connections/AGH-WPA"
+if [ "${WITH_CONFIGURE_AGH_WPA}" = "yes" ]; then
+    echo "# configuring AGH-WPA network"
+    CERT_URL="https://panel.agh.edu.pl/CA-AGH/CA-AGH.der"
+    CERT_DIR="/etc/ca-certificates"
+    AGH_WPA_OUTPUT="/etc/NetworkManager/system-connections/AGH-WPA"
 
-echo -n "   * enter username (login@student.agh.edu.pl): "
-read AGH_WPA_USERNAME
-echo -n "   * enter password: "
-read -s AGH_WPA_PASSWORD
-echo ""
+    echo -n "   * enter username (login@student.agh.edu.pl): "
+    read AGH_WPA_USERNAME
+    echo -n "   * enter password: "
+    read -s AGH_WPA_PASSWORD
+    echo ""
 
-echo -n "   * retrieving MAC..."
-MAC=`ifconfig | grep HWaddr | grep wlan | sed 's/^.*HWaddr //' | sed 's/[ \t]*$//' | tr '[:lower:]' '[:upper:]'`
-echo " ${MAC}"
+    echo -n "   * retrieving MAC..."
+    MAC=`ifconfig | grep HWaddr | grep wlan | sed 's/^.*HWaddr //' | sed 's/[ \t]*$//' | tr '[:lower:]' '[:upper:]'`
+    echo " ${MAC}"
 
-echo -n "   * downloading the certificate to ${CERT_DIR}..."
-pushd . > /dev/null 2>&1
-cd "${CERT_DIR}"
-if [ $? -ne 0 ]; then
+    echo -n "   * downloading the certificate to ${CERT_DIR}..."
+    pushd . > /dev/null 2>&1
+    cd "${CERT_DIR}"
+    if [ $? -ne 0 ]; then
+        popd > /dev/null 2>&1
+        fail "'${CERT_DIR}' does not exist"
+    fi
+
+    rm -f "${CERT_DIR}/CA-AGH.der"
+    wget -q "${CERT_URL}" >> ${INSTALL_LOGFILE} 2>&1
+    if [ $? -ne 0 ]; then
+        popd > /dev/null 2>&1
+        fail "wget '${CERT_URL}' failed"
+    fi
+
     popd > /dev/null 2>&1
-    fail "'${CERT_DIR}' does not exist"
-fi
+    echo " ok"
 
-wget -q "${CERT_URL}" >> ${INSTALL_LOGFILE} 2>&1
-if [ $? -ne 0 ]; then
-    popd > /dev/null 2>&1
-    fail "wget '${CERT_URL}' failed"
-fi
-
-popd > /dev/null 2>&1
-echo " ok"
-
-echo -n "   * creating ${AGH_WPA_OUTPUT}..."
-cat > "${AGH_WPA_OUTPUT}" << EOF
+    echo -n "   * creating ${AGH_WPA_OUTPUT}..."
+    cat > "${AGH_WPA_OUTPUT}" << EOF
 [ipv6]
 method=auto
 
@@ -194,7 +247,8 @@ key-mgmt=wpa-eap
 auth-alg=open
 EOF
 
-echo " ok"
+    echo " ok"
+fi
 
 # commands that do not require root priveleges are in configure.user.sh
 USER_CONFIG_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -211,5 +265,5 @@ do
 done
 
 echo "# executing ${USER_CONFIG_SCRIPT}"
-sudo -u ${LOGNAME} ${LOCAL_VARS} /usr/bin/env bash "${USER_CONFIG_SCRIPT}"
+sudo -u ${LOGNAME} ${LOCAL_VARS} /usr/bin/env bash "${USER_CONFIG_SCRIPT}" "${ALL_ARGUMENTS}"
 
--- configure.user.sh
+++ configure.user.sh
@@ -1,5 +1,75 @@
 #!/usr/bin/env bash
 
+CUSTOM_ARGUMENTS="no"
+
+WITH_DOTFILE_SYMLINKS="no"
+WITH_DOWNLOADS_SYMLINK="no"
+WITH_DROPBOX_SYMLINK="no"
+WITH_OH_MY_ZSH="no"
+WITH_POWERLINE_FONT="no"
+WITH_GNOME_FAVORITES="no"
+WITH_DOWNLOAD_WALLPAPERS="no"
+WITH_RANDOM_WALLPAPER="no"
+WITH_THUNDERBIRD="no"
+WITH_AUTORUN="no"
+WITH_GIT="no"
+
+while [ $# -gt 0 ]; do
+    if [ "$1" = "--dotfile-symlinks" ]; then
+        WITH_DOTFILE_SYMLINKS="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--downloads-symlink" ]; then
+        WITH_DOWNLOADS_SYMLINK="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--dropbox-symlink" ]; then
+        WITH_DROPBOX_SYMLINK="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--oh-my-zsh" ]; then
+        WITH_OH_MY_ZSH="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--powerline-font" ]; then
+        WITH_POWERLINE_FONT="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--gnome-favorites" ]; then
+        WITH_GNOME_FAVORITES="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--download-wallpapers" ]; then
+        WITH_DOWNLOAD_WALLPAPERS="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--random-wallpaper" ]; then
+        WITH_RANDOM_WALLPAPER="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--thunderbird" ]; then
+        WITH_THUNDERBIRD="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--autorun" ]; then
+        WITH_AUTORUN="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "$1" = "--git" ]; then
+        WITH_GIT="yes"
+        CUSTOM_ARGUMENTS="yes"
+    elif [ "${1:0:2}" = "--" ]; then
+        UNKNOWN_ARGUMENTS="${UNKNOWN_ARGUMENTS} $1"
+        CUSTOM_ARGUMENTS="yes"
+    fi
+
+    shift
+done
+
+if [ "${CUSTOM_ARGUMENTS}" = "no" ]; then
+    WITH_DOTFILE_SYMLINKS="yes"
+    WITH_DOWNLOADS_SYMLINK="yes"
+    WITH_DROPBOX_SYMLINK="yes"
+    WITH_OH_MY_ZSH="yes"
+    WITH_POWERLINE_FONT="yes"
+    WITH_GNOME_FAVORITES="yes"
+    WITH_DOWNLOAD_WALLPAPERS="yes"
+    WITH_RANDOM_WALLPAPER="yes"
+    WITH_THUNDERBIRD="yes"
+    WITH_AUTORUN="yes"
+    WITH_GIT="yes"
+fi
+
 function fail {
     if [ $# -eq 0 ]; then
         echo " ERROR"
@@ -11,129 +81,146 @@ function fail {
 }
 
 # make symlinks to dotfiles
-DOTFILES_PATH="${DATA_PARTITION_MOUNT_PATH}/Profiles"
-DOTFILES=`find "${DOTFILES_PATH}" -maxdepth 1 -name '.*'`
+if [ "${WITH_DOTFILE_SYMLINKS}" = "yes" ]; then
+    DOTFILES_PATH="${DATA_PARTITION_MOUNT_PATH}/Profiles"
+    DOTFILES=`find "${DOTFILES_PATH}" -maxdepth 1 -name '.*'`
 
-echo -n "- creating symlinks to dotfiles in ${DOTFILES_PATH}..."
+    echo -n "- creating symlinks to dotfiles in ${DOTFILES_PATH}..."
 
-for DOTFILE in ${DOTFILES}; do
-    DOTFILE_BASENAME=`basename "${DOTFILE}"`
+    for DOTFILE in ${DOTFILES}; do
+        DOTFILE_BASENAME=`basename "${DOTFILE}"`
 
-    rm -rf "${HOME}/${DOTFILE_BASENAME}"
-    ln -s "${DOTFILE}" "${HOME}/"
+        rm -rf "${HOME}/${DOTFILE_BASENAME}"
+        ln -s "${DOTFILE}" "${HOME}/"
 
-    if [ $? -ne 0 ]; then
-        fail "ln -s '${DOTFILE}' '${HOME}/' failed"
-    fi
-done
+        if [ $? -ne 0 ]; then
+            fail "ln -s '${DOTFILE}' '${HOME}/' failed"
+        fi
+    done
 
-echo " ok"
+    echo " ok"
+fi
 
 # symlink to Downloads
-echo -n "- creating a symlink to ${DATA_PARTITION_MOUNT_PATH}/Downloads..."
-if rm -df "${HOME}/Downloads"; then
-    ln -s "${DATA_PARTITION_MOUNT_PATH}/Downloads" "${HOME}/Downloads"
-    echo " ok"
-else
-    fail "rm -df '${HOME}/Downloads' failed"
+if [ "${WITH_DOWNLOADS_SYMLINK}" = "yes" ]; then
+    echo -n "- creating a symlink to ${DATA_PARTITION_MOUNT_PATH}/Downloads..."
+    if rm -df "${HOME}/Downloads"; then
+        ln -s "${DATA_PARTITION_MOUNT_PATH}/Downloads" "${HOME}/Downloads"
+        echo " ok"
+    else
+        fail "rm -df '${HOME}/Downloads' failed"
+    fi
 fi
 
 # symlink to Dropbox folder
-DROPBOX_PATH="${DATA_PARTITION_MOUNT_PATH}/Dropbox"
-DROPBOX_LINK="${HOME}/Dropbox"
+if [ "${WITH_DROPBOX_SYMLINK}" = "yes" ]; then
+    DROPBOX_PATH="${DATA_PARTITION_MOUNT_PATH}/Dropbox"
+    DROPBOX_LINK="${HOME}/Dropbox"
 
-echo -n "- creating a symlink to Dropbox folder..."
-ln -fs "${DROPBOX_PATH}" "${DROPBOX_LINK}"
-if [ $? -eq 0 ]; then
-    echo " ok"
-else
-    fail "ln -s failed"
+    echo -n "- creating a symlink to Dropbox folder..."
+    ln -fs "${DROPBOX_PATH}" "${DROPBOX_LINK}"
+    if [ $? -eq 0 ]; then
+        echo " ok"
+    else
+        fail "ln -s failed"
+    fi
 fi
 
 # install oh-my-zsh
-echo -n "- installing oh-my-zsh..."
-OH_MY_ZSH_URL="git://github.com/robbyrussell/oh-my-zsh.git"
-OH_MY_ZSH_PATH="${HOME}/.oh-my-zsh"
-rm -rf "${OH_MY_ZSH_PATH}"
-git clone "${OH_MY_ZSH_URL}" "${OH_MY_ZSH_PATH}" >> ${INSTALL_LOGFILE} 2>&1
-cp "${OH_MY_ZSH_PATH}/templates/zshrc.zsh-template" "${HOME}/.zshrc"
-echo " ok"
+if [ "${WITH_OH_MY_ZSH}" = "yes" ]; then
+    echo -n "- installing oh-my-zsh..."
+    OH_MY_ZSH_URL="git@github.com:robbyrussell/oh-my-zsh.git"
+    OH_MY_ZSH_PATH="${HOME}/.oh-my-zsh"
+    rm -rf "${OH_MY_ZSH_PATH}"
+    git clone "${OH_MY_ZSH_URL}" "${OH_MY_ZSH_PATH}" >> ${INSTALL_LOGFILE} 2>&1
+    cp "${OH_MY_ZSH_PATH}/templates/zshrc.zsh-template" "${HOME}/.zshrc"
+    echo " ok"
 
-# create a theme based on `agnoster` - add job & SHLVL counter
-echo -n "- creating dextero.zsh-theme..."
-BASE_THEME_PATH="${OH_MY_ZSH_PATH}/themes/agnoster.zsh-theme"
-CUSTOMIZED_THEME_PATH="${OH_MY_ZSH_PATH}/themes/dextero.zsh-theme"
-cp "${BASE_THEME_PATH}" "${CUSTOMIZED_THEME_PATH}"
-vim -u NONE -c 'g/$(jobs/norm f$ct]-n "`jobs`" 2f}a%(1j.f"i.)%(2j. %j.)o  [[ $SHLVL -gt 1 ]] && symbols+="%{%F{green}%}u27f2"yypci"$SHLVLZZ' "${CUSTOMIZED_THEME_PATH}"
-echo " ok"
+    # create a theme based on `agnoster` - add job & SHLVL counter
+    echo -n "- creating dextero.zsh-theme..."
+    BASE_THEME_PATH="${OH_MY_ZSH_PATH}/themes/agnoster.zsh-theme"
+    CUSTOMIZED_THEME_PATH="${OH_MY_ZSH_PATH}/themes/dextero.zsh-theme"
+    cp "${BASE_THEME_PATH}" "${CUSTOMIZED_THEME_PATH}"
+    vim -u NONE -c 'g/$(jobs/norm f$ct]-n "`jobs`" 2f}a%(1j.f"i.)%(2j. %j.)o  [[ $SHLVL -gt 1 ]] && symbols+="%{%F{green}%}u27f2"yypci"$SHLVLZZ' "${CUSTOMIZED_THEME_PATH}"
+    echo " ok"
 
-# customize .zshrc
-echo -n "- customizing .zshrc..."
-# set theme
-vim -u NONE -c 'g/ZSH_THEME=/norm ci"dexteroZZ' "${HOME}/.zshrc"
-# set default user
-echo "DEFAULT_USER=${LOGNAME}" >> ${HOME}/.zshrc
-echo " ok"
-
-# install Powerline-patched fonts
-echo "- installing Powerline-patched fonts"
-mkdir -p "${HOME}/.fonts"
-
-echo "   * ubuntu-mono"
-if [ ! -e "${HOME}/.fonts/ubuntu-mono-powerline-ttf" ]; then
-    git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git "${HOME}/.fonts/ubuntu-mono-powerline-ttf" >> ${INSTALL_LOGFILE} 2>&1
+    # customize .zshrc
+    echo -n "- customizing .zshrc..."
+    # set theme
+    vim -u NONE -c 'g/ZSH_THEME=/norm ci"dexteroZZ' "${HOME}/.zshrc"
+    # set default user
+    echo "DEFAULT_USER=${LOGNAME}" >> ${HOME}/.zshrc
+    echo " ok"
 fi
 
-echo -n "- updating font cache..."
-if fc-cache -vf >> ${INSTALL_LOGFILE} 2>&1 ; then
-    echo " ok"
-else
-    fail " fc-cache failed"
+# install Powerline-patched fonts
+if [ "${WITH_POWERLINE_FONT}" = "yes" ]; then
+    echo "- installing Powerline-patched fonts"
+    mkdir -p "${HOME}/.fonts"
+
+    echo "   * ubuntu-mono"
+    if [ ! -e "${HOME}/.fonts/ubuntu-mono-powerline-ttf" ]; then
+        git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git "${HOME}/.fonts/ubuntu-mono-powerline-ttf" >> ${INSTALL_LOGFILE} 2>&1
+    fi
+
+    echo -n "- updating font cache..."
+    if fc-cache -vf >> ${INSTALL_LOGFILE} 2>&1 ; then
+        echo " ok"
+    else
+        fail " fc-cache failed"
+    fi
 fi
 
 # set Gnome favorites
-echo -n "- replacing apps in Gnome Favorites bar..."
-gsettings set org.gnome.shell favorite-apps "[ 'gnome-terminal.desktop', 'pidgin.desktop', 'google-chrome.desktop', 'thunderbird.desktop', 'nautilus.desktop', 'skype.desktop', 'spotify.desktop' ]" >> ${INSTALL_LOGFILE} 2>&1
-echo " ok"
+if [ "${WITH_GNOME_FAVORITES}" = "yes" ]; then
+    echo -n "- replacing apps in Gnome Favorites bar..."
+    gsettings set org.gnome.shell favorite-apps "[ 'gnome-terminal.desktop', 'pidgin.desktop', 'google-chrome.desktop', 'thunderbird.desktop', 'nautilus.desktop', 'skype.desktop', 'spotify.desktop' ]" >> ${INSTALL_LOGFILE} 2>&1
+    echo " ok"
+fi
 
 # download wallpapers & set random one
-echo "- downloading wallpapers"
-WALLPAPERS_DIR="${HOME}/.config/wallpapers"
-WALLPAPERS=" \
-    http://i.imgur.com/MoWIQcK.jpg \
-    http://i.imgur.com/HKxJDIU.jpg \
-    http://i.imgur.com/owJ7NyD.jpg \
-    http://i.imgur.com/1ohhqXK.jpg \
-    http://i.imgur.com/V2S4f6D.jpg \
-"
+if [ "${WITH_DOWNLOAD_WALLPAPERS}" = "yes" ]; then
+    echo "- downloading wallpapers"
+    WALLPAPERS_DIR="${HOME}/.config/wallpapers"
+    WALLPAPERS=" \
+        http://i.imgur.com/MoWIQcK.jpg \
+        http://i.imgur.com/HKxJDIU.jpg \
+        http://i.imgur.com/owJ7NyD.jpg \
+        http://i.imgur.com/1ohhqXK.jpg \
+        http://i.imgur.com/V2S4f6D.jpg \
+    "
 
-mkdir -p "${WALLPAPERS_DIR}"
-pushd . > /dev/null 2>&1
-cd "${WALLPAPERS_DIR}"
-for URL in ${WALLPAPERS}; do
-    echo "   * ${URL}"
-    wget -q "${URL}" >> ${INSTALL_LOGFILE} 2>&1
-done
-popd > /dev/null 2>&1
+    mkdir -p "${WALLPAPERS_DIR}"
+    pushd . > /dev/null 2>&1
+    cd "${WALLPAPERS_DIR}"
+    for URL in ${WALLPAPERS}; do
+        echo "   * ${URL}"
+        wget -q "${URL}" >> ${INSTALL_LOGFILE} 2>&1
+    done
+    popd > /dev/null 2>&1
+fi
 
-RANDOM_WALLPAPER=`ls "${WALLPAPERS_DIR}"/ | sort -R | head -1`
-echo -n "- setting a random wallpaper (${RANDOM_WALLPAPER})..."
+if [ "${WITH_RANDOM_WALLPAPER}" = "yes" ]; then
+    RANDOM_WALLPAPER=`ls "${WALLPAPERS_DIR}"/ | sort -R | head -1`
+    echo -n "- setting a random wallpaper (${RANDOM_WALLPAPER})..."
 
-gsettings set org.gnome.desktop.background picture-uri "file:///${WALLPAPERS_DIR}/${RANDOM_WALLPAPER}"
-if [ $? -eq 0 ]; then
-    echo " ok"
-else
-    fail
+    gsettings set org.gnome.desktop.background picture-uri "file:///${WALLPAPERS_DIR}/${RANDOM_WALLPAPER}"
+    if [ $? -eq 0 ]; then
+        echo " ok"
+    else
+        fail
+    fi
 fi
 
 # configure Thunderbird profile
-THUNDERBIRD_CONFIG_PATH="${HOME}/.thunderbird"
-THUNDERBIRD_PROFILE_NAME="${LOGNAME}"
-THUNDERBIRD_PROFILE_SOURCE="${DOTFILES_PATH}/Thunderbird/dex"
+if [ "${WITH_THUNDERBIRD}" = "yes" ]; then
+    THUNDERBIRD_CONFIG_PATH="${HOME}/.thunderbird"
+    THUNDERBIRD_PROFILE_NAME="${LOGNAME}"
+    THUNDERBIRD_PROFILE_SOURCE="${DOTFILES_PATH}/Thunderbird/dex"
 
-echo -n "- creating a Thunderbird profile..."
-mkdir -p ${THUNDERBIRD_CONFIG_PATH}
-cat > "${THUNDERBIRD_CONFIG_PATH}/profiles.ini" << EOF
+    echo -n "- creating a Thunderbird profile..."
+    mkdir -p ${THUNDERBIRD_CONFIG_PATH}
+    cat > "${THUNDERBIRD_CONFIG_PATH}/profiles.ini" << EOF
 [General]
 StartWithLastProfile=1
 
@@ -143,41 +230,46 @@ IsRelative=1
 Path=${THUNDERBIRD_PROFILE_NAME}
 
 EOF
-ln -fs "${THUNDERBIRD_PROFILE_SOURCE}" "${THUNDERBIRD_CONFIG_PATH}/${THUNDERBIRD_PROFILE_NAME}"
+    ln -fs "${THUNDERBIRD_PROFILE_SOURCE}" "${THUNDERBIRD_CONFIG_PATH}/${THUNDERBIRD_PROFILE_NAME}"
 
-if [ $? -eq 0 ]; then
-    echo " ok"
-else
-    fail "ln -s failed"
+    if [ $? -eq 0 ]; then
+        echo " ok"
+    else
+        fail "ln -s failed"
+    fi
 fi
 
 # add autorun entries
-AUTOSTART_SHORTCUT_SOURCE="/usr/share/applications"
-AUTOSTART_SHORTCUT_DESTINATION="${HOME}/.config/autostart"
-AUTOSTART_ENTRIES=" \
-    pidgin \
-    thunderbird
-"
+if [ "${WITH_AUTORUN}" = "yes" ]; then
+    AUTOSTART_SHORTCUT_SOURCE="/usr/share/applications"
+    AUTOSTART_SHORTCUT_DESTINATION="${HOME}/.config/autostart"
+    AUTOSTART_ENTRIES=" \
+        pidgin \
+        thunderbird
+    "
 
-echo "- creating autostart entries"
-for ENTRY in ${AUTOSTART_ENTRIES}; do
-    echo "   * ${ENTRY}"
-    cp -f "${AUTOSTART_SHORTCUT_SOURCE}/${ENTRY}.desktop" "${AUTOSTART_SHORTCUT_DESTINATION}/"
-done
+    echo "- creating autostart entries"
+    for ENTRY in ${AUTOSTART_ENTRIES}; do
+        echo "   * ${ENTRY}"
+        cp -f "${AUTOSTART_SHORTCUT_SOURCE}/${ENTRY}.desktop" "${AUTOSTART_SHORTCUT_DESTINATION}/"
+    done
+fi
 
 # configure git
-echo "- configuring git"
-echo -n "   * enter username: "
-read GIT_USERNAME
-git config --global user.name "${GIT_USERNAME}"
+if [ "${WITH_GIT}" = "yes" ]; then
+    echo "- configuring git"
+    echo -n "   * enter username: "
+    read GIT_USERNAME
+    git config --global user.name "${GIT_USERNAME}"
 
-echo -n "   * enter email address: "
-read GIT_EMAIL
-git config --global user.email "${GIT_EMAIL}"
+    echo -n "   * enter email address: "
+    read GIT_EMAIL
+    git config --global user.email "${GIT_EMAIL}"
 
-echo -n "   * enabling colors..."
-if git config --global color.ui true; then
-    echo " ok"
-else
-    fail "git config failed"
+    echo -n "   * enabling colors..."
+    if git config --global color.ui true; then
+        echo " ok"
+    else
+        fail "git config failed"
+    fi
 fi
